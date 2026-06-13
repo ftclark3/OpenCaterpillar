e222abac275e2bc67448f1c5aa5b7db6e77cdd9d
Finley Clark <finn2001clark@gmail.com>
2026-06-13 18:15:26 -0500

    removed awsem-specific code

---
+++ OpenCaterpillar/__init__.py
@@ -0,0 +1,7 @@
+"""An implementation of the Caterpillar coarse-grained protein forcefield in the OpenMM/OpenAWSEM simulation toolkit."""
+from openawsem import *
+from .openCaterpillar import *
+from pathlib import Path
+
+__location__= Path(__file__).resolve().parent
+
--- openawsem/__init__.py
+++
@@ -1,7 +0,0 @@
-"""An implementation of the AWSEM (Associative memory, Water-mediated Structure, and Energy Model) coarse-grained protein forcefield designed for use with the OpenMM simulation toolkit."""
-from .openAWSEM import *
-from pathlib import Path
-
-__location__= Path(__file__).resolve().parent
-from ._version import __version__
-
